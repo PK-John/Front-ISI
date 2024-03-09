74cfc173200ffd9545457901754db65de9445ea1
Victor João <jonh_victor@outlook.com>
2024-03-08 22:52:07 -0300

    Adição dos dados no OverView na tela inicial de consulta

---
+++ assets/js/js_consultas/consulta.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    // Função para fazer a requisição ao backend
+    const fetchData = async (url) => {
+      try {
+        const response = await axios.get(url);
+        return response.data;
+      } catch (error) {
+        console.error("Erro na requisição:", error);
+      }
+    };
+  
+    const updateDado1 = async () => {
+        try {
+          const disciplinasUrl = "http://localhost:5000/disciplinas";
+          const disciplinasData = await fetchData(disciplinasUrl);
+      
+          const quantidadeDisciplinas = disciplinasData.length;
+      
+          const dado2Div = document.querySelector(".dado-1");
+          dado2Div.innerHTML = `
+            <i class='bx bxs-book-alt icon-dado1'></i>
+            <p class="info-text">Total de disciplinas:</p>
+            <p class="quantidade-alunos">${quantidadeDisciplinas}</p>
+          `;
+        } catch (error) {
+          console.error("Erro ao atualizar dado-1:", error);
+        }
+      };
+    
+    const updateDado2 = async () => {
+        try {
+          const alunosUrl = "http://localhost:5000/alunos";
+          const alunosData = await fetchData(alunosUrl);
+      
+          const quantidadeAlunos = alunosData.length;
+      
+          const dado2Div = document.querySelector(".dado-2");
+          dado2Div.innerHTML = `
+            <i class='bx bxs-face icon-dado2'></i>
+            <p class="info-text">Total de Alunos:</p>
+            <p class="quantidade-alunos">${quantidadeAlunos}</p>
+          `;
+        } catch (error) {
+          console.error("Erro ao atualizar dado-2:", error);
+        }
+      };
+  
+    // Chamada da função ao carregar a página
+    updateDado1();
+    updateDado2();
+  });
+  
